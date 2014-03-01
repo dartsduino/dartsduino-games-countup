16f2349f0ba4eaf60375b19d21e7c9f7bd32e83f
Ikuo Terado <eqobar@gmail.com>
2014-03-01 15:50:52 +0900

    Initial commit

---
+++ lib/dartsduino/games/countup.rb
@@ -0,0 +1,9 @@
+require "dartsduino/games/countup/version"
+
+module Dartsduino
+  module Games
+    module Countup
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dartsduino/games/countup/version.rb
@@ -0,0 +1,7 @@
+module Dartsduino
+  module Games
+    module Countup
+      VERSION = "0.0.1"
+    end
+  end
+end
